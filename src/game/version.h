41f6ce8a0a14e4e8e9abd6b7a135f42875ce6246
ST-Chara <74450074+ST-Chara@users.noreply.github.com>
2022-12-21 19:58:06 +0800

    missed ' " " '

--- src/game/version.h
+++ src/game/version.h
@@ -1,7 +1,7 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
-#define FORCE_GAME_NETVERSION_HASH 75ab95347fcdeac2
+#define FORCE_GAME_NETVERSION_HASH "75ab95347fcdeac2"
 #define GAME_VERSION "0.5.1"
 #define GAME_NETVERSION "0.5.1" FORCE_GAME_NETVERSION_HASH
 static const char GAME_RELEASE_VERSION[8] = "0.5.1";
